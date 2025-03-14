9a2c988f44c74926358e53e354a1241723fa99d0
siddshashi <siddshashi5@gmail.com>
2025-03-14 17:21:26 +0100

    changed base URL based on backend deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL
 const api = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "http://34.59.183.102:8000"
 });
 
 // Export the Axios instance
